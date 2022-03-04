8e799128bb3709ee12ff7a07fc399d9b808e4441
moriarty83 <moriarty83@gmail.com>
2022-03-03 21:37:16 -0500

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>I'm Learning Vue</h1>
+    <input type="text" placeholder="Filter Search" v-model="query" />
+    <p>{{query}}</p>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup(){
+    const query = ref("");
+
+    const reset = (evt) =>{
+      query.value = "";
+    }
+    return {
+      query,
+      reset,
+    };
+  }
+}
+</script>
